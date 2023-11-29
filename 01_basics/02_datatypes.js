625f6e4876adf087a2b587bbc198ab245acc8e7e
Raja-Malli-Dileep-Vadde <152243743+Raja-Malli-Dileep-Vadde@users.noreply.github.com>
2023-11-29 05:21:57 +0000

    datatypes(2.1)

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,41 @@
+"use strict"; /* To treat all JS code as newer version (latest) */
+
+//alert(3 +3) /* alert is not defined because we are using node.js, not browser */
+
+
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Dileep")    
+
+
+
+let name = "Dileep"
+let age = 25
+let isLoggedIn = false 
+let state = "" /* it is not null */
+
+
+
+
+// number => 2 to power 53 (range)
+// bigint
+// string => "",''
+// boolean => true/flase
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof "dileep")
+console.log(typeof age)
+console.log(typeof null) /* object */
+console.log(typeof undefined) /* undefined */
+
+
+
