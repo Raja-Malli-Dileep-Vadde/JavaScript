dba587c749f4692b29b9b86b167a10fe6a13e4bd
Raja-Malli-Dileep-Vadde <152243743+Raja-Malli-Dileep-Vadde@users.noreply.github.com>
2024-03-05 11:15:44 +0000

    comparisonDataTypes

---
+++ 01_basics/04_comparisonDataTypes.js
@@ -0,0 +1,13 @@
+// console.log(null <= 0)
+// console.log(null == undefined)
+// console.log(NaN == 0)
+// console.log("2" == 2)
+// console.log("2" === 2)
+console.log(null === undefined)
+console.log("abc"  == "ABC")
+console.log("1" > 1)
+console.log("1" < 1)
+console.log("1" <= 1)
+
+/* here null == 0, it converts null to number then value is NaN So, it is false, this occurs only in equals (==) case only not in === 
+*/
