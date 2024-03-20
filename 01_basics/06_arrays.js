c30334b4fcd4a07ea858dafbd2ebe050a8787f24
Raja-Malli-Dileep-Vadde <152243743+Raja-Malli-Dileep-Vadde@users.noreply.github.com>
2024-03-20 04:18:59 +0000

    arrray

---
+++ 01_basics/06_arrays.js
@@ -0,0 +1,7 @@
+// Array
+
+/*   it is a object, it is collection multiple elements which stored in single variable of same or different DataTypes  */
+
+const myArray = [1,2,3,4,5,6] /* one way for array declaration */
+
+
